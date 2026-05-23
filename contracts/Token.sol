3d4ee7624356d37aadfc51549afdba9067e67362
Moustafa KOTERBA <moustafa.koterba@ledger.fr>
2026-05-23 17:59:58 +0200

    feat: init project and setup token definition

---
+++ contracts/Token.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+// This line is required by Solidity. It declares the open-source license.
+// The compiler will warn if it's missing.
+
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC20, Ownable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20(name, symbol) Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount * 10 ** decimals());
+    }
+}
